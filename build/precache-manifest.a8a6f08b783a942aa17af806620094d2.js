2696a8a55de5ff731ef1604ef59b18f40654e7ca
danielnazario <danielnazario@krowdy.co>
2020-03-23 08:16:11 -0500

    commit 1

---
+++ build/precache-manifest.a8a6f08b783a942aa17af806620094d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf16b2737b942d86b764ad62b879fd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2a56205cc134456b531",
+    "url": "/static/css/main.fe77c462.chunk.css"
+  },
+  {
+    "revision": "f31da25bfee5bc8c315f",
+    "url": "/static/js/2.19a70450.chunk.js"
+  },
+  {
+    "revision": "432ed034d9831dc13cf75201c058fd1d",
+    "url": "/static/js/2.19a70450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a56205cc134456b531",
+    "url": "/static/js/main.f83ffc4e.chunk.js"
+  },
+  {
+    "revision": "f0f446e77ebceccd22d4",
+    "url": "/static/js/runtime-main.9df7dbe6.js"
+  },
+  {
+    "revision": "7ca421388bd9ff5b8a2610a34d4b9166",
+    "url": "/static/media/fondo.7ca42138.svg"
+  }
+]);
