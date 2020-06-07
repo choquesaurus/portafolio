6c1ed3b909c9d515e4ed22d08765d946335ca737
wasauskyok <daniechoque159@gmail.com>
2020-06-07 10:38:38 -0500

    add metadescription with helmet

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Communityandfotosexpasion from './community_and_fotos_exapansion'
+import {Helmet} from 'react-helmet'
 export default function  Community(){
     
-    return <Communityandfotosexpasion/> 
+    return <>
+     <Helmet>      
+            <meta name='description' content='En la seccion de comunidad encontraras las fotos de las personas que forman parte de la comunidad , eventos y amigos relacionados ala tecnologia'/>
+            <title>Daniel | Comunidad</title>       
+        </Helmet>
+    <Communityandfotosexpasion/> 
+    </>
 }
