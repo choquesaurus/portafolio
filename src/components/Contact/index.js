fef492ca2e4f2f9f8963214fa74ef23a7c4092da
choquesaurus <daniechoque159@gmail.com>
2020-09-28 00:18:18 -0500

    quit comments

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,30 +1,22 @@
-import React,{useRef} from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { Typography, Tooltip } from '@material-ui/core';
-import {ToastContainer,toast} from  'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios'
+import React, { useRef } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { Typography, Tooltip } from "@material-ui/core";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 export default function FormContact() {
   const [open, setOpen] = React.useState(false);
-  const [emailerror,setEmailError] = React.useState(false);
-  const [msjerror,setmsjError] = React.useState(false);
-  const email=useRef('');
-  const msj=useRef('');
-  // const [obj,setObj]=React.useState({
-  //   address:'',
-  // })
-  // const CambioData = async (e)=>{
-    //   await setObj({...obj,
-    //   mensaje:''
-  //     [e.target.name]:e.target.value
-  //   })
-  // }
+  const [emailerror, setEmailError] = React.useState(false);
+  const [msjerror, setmsjError] = React.useState(false);
+  const email = useRef("");
+  const msj = useRef("");
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -32,165 +24,163 @@ export default function FormContact() {
     setOpen(false);
   };
 
-  const SendMail= async() =>{
-    
-  let mensaje=msj.current.value;
-  let correo=email.current.value; 
-  if(!( mensaje.length > 0 ) || !(correo.length >0)){
- 
-  if( !( mensaje.length > 0 )){
-        await setmsjError(true)
-         setTimeout(()=>{
+  const SendMail = async () => {
+    let mensaje = msj.current.value;
+    let correo = email.current.value;
+    if (!(mensaje.length > 0) || !(correo.length > 0)) {
+      if (!(mensaje.length > 0)) {
+        await setmsjError(true);
+        setTimeout(() => {
           //setmsjError(false)
-           setmsjError(false);
-        },600);
-  }
-  if(!( correo.length > 0)){
-    await setEmailError(true)
-     setTimeout(()=>{
-      //setmsjError(false)
-       setEmailError(false);
-    },600);
-  }
-}
-  else{
-  try {
-      await handleClose();
-      await toast.info("Enviando mensaje ... ", {
-      position: "top-center",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true
-      });
+          setmsjError(false);
+        }, 600);
+      }
+      if (!(correo.length > 0)) {
+        await setEmailError(true);
+        setTimeout(() => {
+          //setmsjError(false)
+          setEmailError(false);
+        }, 600);
+      }
+    } else {
+      try {
+        await handleClose();
+        await toast.info("Enviando mensaje ... ", {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
 
-
-      //const request = await fetch('https://portafolio-backend.herokuapp.com/send',{
-    const {data:{message}} = await axios.post('https://portafolio-functions-backend.vercel.app/api/msj',{msj:mensaje,email:correo})
-    // {headers:{"Access-Control-Allow-Origin":'*',"Access-Control-Allow-Credentials":true}})
-    //console.log(data)  
-    // const request = await fetch('https://portafolio-functions-backend.vercel.app/api/msj',{
-    // method:'POST',
-    //   headers:{
-    //      //"Access-Control-Allow-Origin":'*',
-    //      //"Access-Control-Allow-Credentials":true,
-    //     "content-type":"application/json"
-    //   },
-    //   body:JSON.stringify({msj:mensaje,email:correo})
-    // })
-    //   const {message} = await request.json();
-      await toast.success(""+message, {
-      position: "top-center",
-      autoClose: 4000,
-      closeOnClick: true,
-      hideProgressBar: false,
-      pauseOnHover: true,
-      draggable: true
-      });
-      // msj.current.value=''
-      // email.current.value=''
-  }   catch (error) {
-      console.log(error)
-      await toast.error(""+error, {
-      position: "top-center",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true
-      });
-  }
-}
-  }
+        //const request = await fetch('https://portafolio-backend.herokuapp.com/send',{
+        const {
+          data: { message },
+        } = await axios.post(
+          "https://portafolio-functions-backend.vercel.app/api/msj",
+          { msj: mensaje, email: correo }
+        );
+        // {headers:{"Access-Control-Allow-Origin":'*',"Access-Control-Allow-Credentials":true}})
+        //console.log(data)
+        // const request = await fetch('https://portafolio-functions-backend.vercel.app/api/msj',{
+        // method:'POST',
+        //   headers:{
+        //      //"Access-Control-Allow-Origin":'*',
+        //      //"Access-Control-Allow-Credentials":true,
+        //     "content-type":"application/json"
+        //   },
+        //   body:JSON.stringify({msj:mensaje,email:correo})
+        // })
+        //   const {message} = await request.json();
+        await toast.success("" + message, {
+          position: "top-center",
+          autoClose: 4000,
+          closeOnClick: true,
+          hideProgressBar: false,
+          pauseOnHover: true,
+          draggable: true,
+        });
+      } catch (error) {
+        console.log(error);
+        await toast.error("" + error, {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+      }
+    }
+  };
   return (
     <div>
-      <ToastContainer/>
-        <Tooltip title='Contactame' arrow>
-            <Typography onClick={handleClickOpen} style={{cursor:'pointer'}} className="animate__animated animate__fast  animate__fadeInDown">
-                Contacto
-            </Typography>
-        </Tooltip>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-  
+      <ToastContainer />
+      <Tooltip title="Contactame" arrow>
+        <Typography
+          onClick={handleClickOpen}
+          style={{ cursor: "pointer" }}
+          className="animate__animated animate__fast  animate__fadeInDown"
+        >
+          Contacto
+        </Typography>
+      </Tooltip>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
         <DialogTitle id="form-dialog-title">Contáctame</DialogTitle>
         <DialogContent>
           <DialogContentText>
-          {/* {
-            obj.address+" , "+obj.mensaje
-          } */}
-          Para suscribirse a este sitio web, ingrese su dirección de correo electrónico y luego su mensaje aquí . Le enviaremos actualizaciones de vez en cuando.
-          
+            Para suscribirse a este sitio web, ingrese su dirección de correo
+            electrónico y luego su mensaje aquí . Le enviaremos actualizaciones
+            de vez en cuando.
           </DialogContentText>
-          {
-            emailerror === true ?
+          {emailerror === true ? (
             <TextField
-            fullWidth
-            autoComplete="off"
-            error 
-            label="Ingresa tu correo . . ."
-            id = "standard-error-helper-text" 
-            helperText="Campo obligatorio !"
-            />
-            :
-            <TextField
-            //onChange={CambioData}
-            inputRef={email}
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            //name='address'
-            fullWidth
-            //required
-            />
-            }
-
-            {
-              msjerror === true ? 
-              <TextField
               fullWidth
               autoComplete="off"
-              error 
-              label="Ingresa tu mensaje . . ."
-              id = "standard-error-helper-text" 
+              error
+              label="Ingresa tu correo . . ."
+              id="standard-error-helper-text"
               helperText="Campo obligatorio !"
-          />
-          :
+            />
+          ) : (
+            <TextField
+              //onChange={CambioData}
+              inputRef={email}
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              //name='address'
+              fullWidth
+              //required
+            />
+          )}
 
-          <TextField
-          //onChange={CambioData}
-          inputRef={msj}
-          margin="dense"
-          id="message"
-          label="Ingrese un pequeño mensaje"
-          //name='mensaje'
-          fullWidth
-          autoComplete="off"
-          //required
-        />
-            }
-       
+          {msjerror === true ? (
+            <TextField
+              fullWidth
+              autoComplete="off"
+              error
+              label="Ingresa tu mensaje . . ."
+              id="standard-error-helper-text"
+              helperText="Campo obligatorio !"
+            />
+          ) : (
+            <TextField
+              //onChange={CambioData}
+              inputRef={msj}
+              margin="dense"
+              id="message"
+              label="Ingrese un pequeño mensaje"
+              //name='mensaje'
+              fullWidth
+              autoComplete="off"
+              //required
+            />
+          )}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel {msjerror}
           </Button>
-          {
-            msjerror === true || emailerror === true?
-            <Button  color="primary" onClick={SendMail} disabled>
-            Enviar
-          </Button>
-          :
-              <Button  color="primary" onClick={SendMail}>
+          {msjerror === true || emailerror === true ? (
+            <Button color="primary" onClick={SendMail} disabled>
               Enviar
             </Button>
-          }
-          
+          ) : (
+            <Button color="primary" onClick={SendMail}>
+              Enviar
+            </Button>
+          )}
         </DialogActions>
-        
-       {/* <form method='POST' action='https://formspree.io/daniechoque159@gmail.com'>
+
+        {/* <form method='POST' action='https://formspree.io/daniechoque159@gmail.com'>
         <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
         <DialogContent>
           <DialogContentText>
--- src/components/Proyects/index.js
+++ src/components/Proyects/index.js
@@ -1,62 +1,75 @@
-import React from 'react'
-import Cardproyects from './Carproyects'
-import Esqueleto from './skeleton'
-import {Helmet} from  'react-helmet'
-import axios from 'axios'
-export default function  Proyects(){
-    const [proyects,setproyect]=React.useState([])
-    const [load,setloading]=React.useState(false)
-    
-    React.useEffect( ()=>{
-         godata()
-    },[])
-    
-    async function godata(){
-        await setloading(true)
+import React from "react";
+import Cardproyects from "./Carproyects";
+import Esqueleto from "./skeleton";
+import { Helmet } from "react-helmet";
+import axios from "axios";
+export default function Proyects() {
+  const [proyects, setproyect] = React.useState([]);
+  const [load, setloading] = React.useState(false);
 
-        //const request=await fetch('https://portafolio-backend.herokuapp.com/proyects');
-        ///const request=await fetch('https://portafolio-functions-backend.vercel.app/api/lista');
+  React.useEffect(() => {
+    godata();
+  }, []);
 
-        //const data=await request.json()
-        const {data} = await axios.get('https://portafolio-functions-backend.vercel.app/api/lista')
-        //console.log(data)
-        await setloading(false)
-        setproyect(data)
-    }
+  async function godata() {
+    await setloading(true);
 
-    // const ScrollFunctions=(params)=>{
-    //     let scrolltop=params.target.scrollTop;
-    //     var offsetTop = document.getElementById('padresito').offsetTop;//cuantos pixeles existen la distancia desde el inicio del body alta el inicio del div 
-    //     var height_div = document.getElementById('padresito').offsetHeight;
-    //     //var clientHeight = document.getElementById('padresito').clientHeight;
-        
-    //     console.log("Hellow",params.target.scrollTop)
-    //    }
+    //const request=await fetch('https://portafolio-backend.herokuapp.com/proyects');
+    ///const request=await fetch('https://portafolio-functions-backend.vercel.app/api/lista');
 
+    //const data=await request.json()
+    const { data } = await axios.get(
+      "https://portafolio-functions-backend.vercel.app/api/lista"
+    );
+    //console.log(data)
+    await setloading(false);
+    setproyect(data);
+  }
 
-    return <div 
-    className="proyects"
-    //onScroll={ScrollFunctions}
-    >    
-         <Helmet>
-            <meta name='description' content='En la seccion de proyectos encontraras mis ejemplos de prueba o experimentación y proyectos que eh realizado '/>
-            <title>Daniel | Proyectos </title>
-        </Helmet>
-        {
-            load === true?
-            <div style={{width:'100%',height:'100%',display:'flex',flexWrap:'wrap'}}>
-               {[...Array(20).keys()].map(item=>{
-                   return <Esqueleto  key={item.toString()}/>
-                })}
-            </div>
-            :
-            proyects.reverse().map(({_id,image,title,description,repository,goproyect},indice,)=>(
-                <Cardproyects   key={_id} 
-                    {...{title,image,description,repository,goproyect,indice}}
-                //  title={title} image={image} description={description} repository={repository} goproyect={goproyect}
-                  />
-                ))
-        }
-      
+  return (
+    <div className="proyects">
+      <Helmet>
+        <meta
+          name="description"
+          content="En la seccion de proyectos encontraras mis ejemplos de prueba o experimentación y proyectos que eh realizado "
+        />
+        <title>Daniel | Proyectos </title>
+      </Helmet>
+      {load === true ? (
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          {[...Array(20).keys()].map((item) => {
+            return <Esqueleto key={item.toString()} />;
+          })}
+        </div>
+      ) : (
+        proyects
+          .reverse()
+          .map(
+            (
+              { _id, image, title, description, repository, goproyect },
+              indice
+            ) => (
+              <Cardproyects
+                key={_id}
+                {...{
+                  title,
+                  image,
+                  description,
+                  repository,
+                  goproyect,
+                  indice,
+                }}
+              />
+            )
+          )
+      )}
     </div>
+  );
 }
